dc37d6d9f5e987c6da9cad11cc03184eda7f4edd
stefanocudini <stefano.cudini@gmail.com>
2014-09-26 16:53:58 +0200

    map in directories

--- directories/main.js
+++ directories/main.js
@@ -4,6 +4,13 @@ var fx_controller = (function() {
 
     initLogin();
   
+    var frameUrl = "http://fenixapps.fao.org/maps/api?"+
+      "baselayers=mapquest&layers=gaul0_faostat_3857&styles=join&joincolumn=iso3_code"+
+      "&lat=0&lon=20&zoom=4"+
+      "&joindata=[(EGY,1),(GHA,0),(NGR,1),(MOZ,1)]"+
+      "&enablejoingfi=true&legendtitle=fertilizers&mu=fertilizers&lang=E&colors=238B45,ffffff&ranges=1&classification=custom";
+    
+    $('#mapFrame').attr({src: frameUrl});
   };
 
   function initLogin(){

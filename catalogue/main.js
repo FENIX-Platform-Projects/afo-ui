7ec7d91e772a84dce8e3510b87c94d62cb149dc8
stefanocudini <stefano.cudini@gmail.com>
2014-09-28 11:48:51 +0200

    map by fert name

--- catalogue/main.js
+++ catalogue/main.js
@@ -1,10 +1,75 @@
+
 var fx_controller = (function() {
 
-  function init(){
+  var $frameMap = $('#frameMap'),
+      $listFertilizers = $('#listFertilizers');
+
+  function init() {
 
     initLogin();
-  
+    initMap();
+    initFertilizers();
+
   };
+
+  function initFertilizers() {
+
+    for(var i in Fertilizers)
+     $listFertilizers.append('<option value="'+Fertilizers[i]+'">'+Fertilizers[i]+'</option>');
+
+    $listFertilizers.on('change', function() {
+        var fert = this.value;
+        console.log(fert);
+        initMap(fert);
+    });
+  }
+
+  function initMap(fert) {
+
+    //fert: DAP,MKP,MOP,MRP
+
+      var query = "SELECT country FROM countries WHERE name = '"+ fert +"' AND value=1",
+          data = {};
+
+      //CREATE TABLE countries (s varchar(60), name varchar(60), country varchar(3), value INT);
+
+      data.datasource = 'africanfertilizers';
+      data.thousandSeparator = ',';
+      data.decimalSeparator = '.';
+      data.decimalNumbers = 2;
+      data.json = JSON.stringify({query: query});
+      data.cssFilename = '';
+      data.nowrap = false;
+      data.valuesIndex = 0;
+
+      $.ajax({
+          type    :   'POST',
+          url     :   'http://faostat3.fao.org/wds/rest/table/json',
+          data    :   data,
+          success: function (resp) {
+
+            console.log(resp);
+
+            var ccodes = _.map(resp, function(val) {
+              return "("+val[0]+",1)";
+            }).join(',');
+
+            console.log(ccodes);
+
+            var frameUrl = "http://fenixapps.fao.org/maps/api?"+
+              "baselayers=mapquest&layers=gaul0_faostat_3857&styles=join&joincolumn=iso3_code"+
+              "&lat=0&lon=20&zoom=4"+
+              "&joindata=["+ccodes+"]"+  //[(EGY,1),(GHA,0),(NGR,1),(MOZ,1)]
+              "&enablejoingfi=true&legendtitle=fertilizers&mu=fertilizers&lang=E&colors=238B45,ffffff&ranges=1&classification=custom";
+            
+            $('#frameMap').attr({src: frameUrl });
+
+          },
+          error: function (e, b, c) {
+          }
+      });
+    
+  }
 
   function initLogin(){
 
@@ -21,3 +86,4 @@ var fx_controller = (function() {
 })();
 
 window.addEventListener('load', fx_controller.init, false);
+
--- directories/main.js
+++ directories/main.js
@@ -6,43 +6,6 @@ var fx_controller = (function() {
 
 //TODO MAKE SELECT BOX FROM fertilizers.js
 
-var fert = 'DAP';
-
-    var data = {},
-        q = "SELECT * FROM countries WHERE name = '"+ fert +"'";
-
-    data.datasource = 'africanfertilizers';
-    data.thousandSeparator = ',';
-    data.decimalSeparator = '.';
-    data.decimalNumbers = 2;
-    
-    //CREATE TABLE countries (s varchar(60), name varchar(60), country varchar(3), value INT);
-    
-    data.json = JSON.stringify({query: q});
-    data.cssFilename = '';
-    data.nowrap = false;
-    data.valuesIndex = 0;
-
-    $.ajax({
-        type    :   'POST',
-        url     :   'http://faostat3.fao.org/wds/rest/table/json',
-        data    :   data,
-        success: function (resp) {
-            console.log(resp);
-        },
-        error: function (e, b, c) {
-        }
-    });
-  
-    var frameUrl = "http://fenixapps.fao.org/maps/api?"+
-      "baselayers=mapquest&layers=gaul0_faostat_3857&styles=join&joincolumn=iso3_code"+
-      "&lat=0&lon=20&zoom=4"+
-      "&joindata=[(EGY,1),(GHA,0),(NGR,1),(MOZ,1)]"+
-      "&enablejoingfi=true&legendtitle=fertilizers&mu=fertilizers&lang=E&colors=238B45,ffffff&ranges=1&classification=custom";
-    
-    $('#mapFrame').attr({src: frameUrl});
-  };
-
   function initLogin() {
 
     $('.protected').hide();

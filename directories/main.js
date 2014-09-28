81d30e3d48eda5ebc52895df7ff63c913c42dbbd
stefanocudini <stefano.cudini@gmail.com>
2014-09-28 16:02:49 +0200

    directory companies

--- directories/main.js
+++ directories/main.js
@@ -1,8 +1,38 @@
+
 var fx_controller = (function() {
+
+
+  var $listCountries = $('#listCountries'),
+  	  $listCompanies =$('#listCompanies');
 
   function init() {
 
     initLogin();
+    initCountries();
+
+  };
+
+  function initCountries() {
+
+    $listCountries.on('click', 'a', function(e) {
+    	$(this).siblings().removeClass('btn-success active');
+    	$(this).addClass('btn-success active');
+    	selectCompany({country: $(this).text() });
+    });
+  }
+
+  function selectCompany(filter) {
+  	//hide all
+  	$listCompanies.find('.tabCompany').hide().each(function() {
+  		if( $(this).find('td:eq(11)').text() === filter.country)
+  			$(this).show().find('tr:eq(5)').addClass('bg-success');
+  	});
+
+
+  }
+
+    
+
 
 //TODO MAKE SELECT BOX FROM fertilizers.js
 

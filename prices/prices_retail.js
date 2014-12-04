5cc2b17565fcc7c33c1e4c183d7a41f14ab0230f
stefanocudini <stefano.cudini@gmail.com>
2014-12-04 14:34:59 +0100

    markers olap

--- prices/prices_retail.js
+++ prices/prices_retail.js
@@ -6,52 +6,62 @@ var map = L.map('prices_retail_map', {
 		layers: L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
 	});
 
-var layerRetail = new L.MarkerClusterGroup();//L.featureGroup();
+var layerRetail = new L.MarkerClusterGroup({
+	maxClusterRadius:30
+});
 layerRetail.addTo(map);
 
 $('#prices_retail_grid').load("html/prices_retail.html", function() {
-	
-	var places = [];
+
+	function loadMarkers() {
+
+		var places = [],
+			placesquery = [];
+
+		$('#prices_retail_grid tbody tr.success').each(function() {
+
+				var fert = $(this).find('td').eq(4).text(),
+					price = $(this).find('td').eq(6).text(),
+					place = $(this).find('td').eq(0).text()+', '+
+							$(this).find('td').eq(3).text();
+							
+				places.push({
+					fert: fert,
+					place: place,
+					loc: []
+				});
+			});
+
+			placesquery = _.pluck(places, 'place').join('|');
+
+			layerRetail.clearLayers();
+			$.get('http://168.202.28.214:5020/spatialquery/geocoding/'+placesquery, function(json) {
+
+				var bb = L.latLngBounds(json);
+
+				for(var i in json)
+				{
+					L.marker(L.latLng(json[i]), {
+						title: places[i].place
+					})
+					.bindPopup([
+						'<b>'+places[i].place+'</b>',
+						places[i].fert,
+						places[i].price
+						].join('<br>')
+					).addTo(layerRetail);
+				}
+
+				map.fitBounds( bb.pad(1) );
+			});
+	}
 
 	$('#prices_retail_grid tbody tr').on('click', function(e) {
 		$(this).toggleClass('success');
 
-		$('#prices_retail_grid tbody tr.success').each(function() {
-
-			var fert = $(this).find('td').eq(4).text(),
-				price = $(this).find('td').eq(6).text(),
-				place = $(this).find('td').eq(0).text()+', '+
-						$(this).find('td').eq(3).text();
-			
-			places.push({
-				fert: fert,
-				place: place,
-				loc: []
-			});
-		});
-
-		var placesquery = _.pluck(places, 'place').join('|');
-
-		layerRetail.clearLayers();
-		$.get('http://168.202.28.214:5020/spatialquery/geocoding/'+placesquery, function(json) {
-
-			var bb = L.latLngBounds(json);
-
-			for(var i in json)
-			{
-				L.marker(L.latLng(json[i]), {
-					title: places[i].place
-				})
-				.bindPopup([
-					'<b>'+places[i].place+'</b>',
-					places[i].fert,
-					places[i].price
-					].join('<br>')
-				).addTo(layerRetail);
-			}
-
-			map.fitBounds( bb.pad(1) );
-		});
+		loadMarkers();
 	});
+
+	loadMarkers();
 });
 

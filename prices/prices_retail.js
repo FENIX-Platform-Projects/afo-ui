e36f712cb7c43d13d395c7816433444429d52537
stefanocudini <stefano.cudini@gmail.com>
2014-12-04 13:24:08 +0100

    multiple geocoding

--- prices/prices_retail.js
+++ prices/prices_retail.js
@@ -11,48 +11,47 @@ layerRetail.addTo(map);
 
 $('#prices_retail_grid').load("html/prices_retail.html", function() {
 	
-	var geocoder = new google.maps.Geocoder();
+	var places = [];
 
-	$('#prices_retail_grid tr').on('click', function(e) {
+	$('#prices_retail_grid tbody tr').on('click', function(e) {
 		$(this).toggleClass('success');
 
-		$('#prices_retail_grid tr.success').each(function() {
+		$('#prices_retail_grid tbody tr.success').each(function() {
 
 			var fert = $(this).find('td').eq(4).text(),
 				price = $(this).find('td').eq(6).text(),
 				place = $(this).find('td').eq(0).text()+', '+
 						$(this).find('td').eq(3).text();
-
-			layerRetail.clearLayers();
-
-			//geocoder.geocode({address: place}, function (rawjson) {
-			$.get('http://168.202.28.214:5020/spatialquery/geocoding/'+place, function(json) {
-
-				if(json) {
-					loc = L.latLng(json);
-					var pop = ['<b>'+place+'</b>',fert,price].join('<br>');
-					L.marker(loc,{title: place}).bindPopup(pop).addTo(layerRetail);
-
-/*					console.log(json);
-					var json = {},
-						key, loc, disp = [];
-
-					for(var i in rawjson)
-					{
-						key = rawjson[i].formatted_address;
-						
-						loc = L.latLng( rawjson[i].geometry.location.lat(), rawjson[i].geometry.location.lng() );
-						var pop = ['<b>'+place+'</b>',fert,price].join('<br>');
-						L.marker(loc,{title: place}).bindPopup(pop).addTo(layerRetail);
-					}*/
-				}
-
+			
+			places.push({
+				fert: fert,
+				place: place,
+				loc: []
 			});
-			setTimeout(function() {
-				map.fitBounds(layerRetail.getBounds().pad(1));
-			},800);
 		});
 
+		var placesquery = _.pluck(places, 'place').join('|');
+
+		layerRetail.clearLayers();
+		$.get('http://168.202.28.214:5020/spatialquery/geocoding/'+placesquery, function(json) {
+
+			var bb = L.latLngBounds(json);
+
+			for(var i in json)
+			{
+				L.marker(L.latLng(json[i]), {
+					title: places[i].place
+				})
+				.bindPopup([
+					'<b>'+places[i].place+'</b>',
+					places[i].fert,
+					places[i].price
+					].join('<br>')
+				).addTo(layerRetail);
+			}
+
+			map.fitBounds( bb.pad(1) );
+		});
 	});
 });
 

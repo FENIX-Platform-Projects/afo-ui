16fdef594c5bb71f7c8573a79248036ca4f492b2
stefanocudini <stefano.cudini@gmail.com>
2014-12-09 15:25:41 +0100

    directories.html work

--- directories.js
+++ directories.js
@@ -1,53 +1,79 @@
 
-var fx_controller = (function() {
+require.config({
 
+	baseUrl: 'src/',
 
-  var $listCountries = $('#listCountries'),
-  	  $listCompanies =$('#listCompanies');
+	paths: {
+		'i18n'                  :'lib/i18n',
+		'text'                  :'lib/text',
+		'domready'              :'lib/domready',
+		'bootstrap'             :'lib/bootstrap',	
+		//'backbone'              :'//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min',
+		'highcharts'            :'//fenixapps.fao.org/repository/js/highcharts/4.0.4/js/highcharts',
+		//'highcharts_exporting'  :'//fenixapps.fao.org/repository/js/highcharts/4.0.4/js/modules/exporting',
+		//'highcharts-heatmap'    :'http://code.highcharts.com/maps/modules/heatmap',
+		//'highcharts-data'       :'http://code.highcharts.com/maps/modules/data',
+		'jquery'                :'lib/jquery',
+		'underscore'            :'lib/underscore',
+		'jstree'                :'lib/jstree/jstree.min',
+		'handlebars'            :'lib/handlebars',
 
-  function init() {
+		//fenix-map-js
+		'fenix-map'             :'fenix_modules/fenix-map-js/fenix-map-min',
+		'fenix-map-config'      :'fenix_modules/fenix-map-js/fenix-map-config',		
+		'chosen'                :'//fenixapps.fao.org/repository/js/chosen/1.0.0/chosen.jquery.min',		
+		'leaflet'               :'//fenixapps.fao.org/repository/js/leaflet/0.7.3/leaflet',	    
+		'import-dependencies'   :'//fenixapps.fao.org/repository/js/FENIX/utils/import-dependencies-1.0',
+		'jquery.power.tip'      :'//fenixapps.fao.org/repository/js/jquery.power.tip/1.1.0/jquery.powertip.min',
+		'jquery-ui'             :'//fenixapps.fao.org/repository/js/jquery-ui/1.10.3/jquery-ui-1.10.3.custom.min',
+		'jquery.i18n.properties':'//fenixapps.fao.org/repository/js/jquery/1.0.9/jquery.i18n.properties-min',
+		'jquery.hoverIntent'    :'//fenixapps.fao.org/repository/js/jquery.hoverIntent/1.0/jquery.hoverIntent',
+		'fenix-ui-topmenu'      :'../scripts/components/fenix-ui-topmenu'
+	},
 
-    initLogin();
-    initCountries();
+	shim: {
+		'bootstrap' : ['jquery'],
+		'chosen'    : ['jquery'],
+		'highcharts': ['jquery'],
+		'jstree'    : ['jquery'],
+		'jquery-ui'             : ['jquery'],
+		'jquery.power.tip'      : ['jquery'],
+		'jquery.i18n.properties': ['jquery'],
+		'jquery.hoverIntent'    : ['jquery'],
+		'underscore': {
+		    exports: '_'
+		}
+	}
+});
 
-  };
+require([
+	'jquery','underscore','bootstrap','highcharts','jstree','handlebars',
+	'text!html/accordion.html',
+	'fenix-ui-topmenu/main',
+	'domready!'
+], function($,_,bts,highcharts,jstree,Handlebars,
+	accordion, TopMenu) {
 
-  function initCountries() {
+	new TopMenu({
+		url: 'json/fenix-ui-topmenu_config.json',
+		active: "directories"
+	});
 
-    $listCountries.on('click', 'a', function(e) {
-    	$(this).siblings().removeClass('btn-success active');
-    	$(this).addClass('btn-success active');
-    	selectCompany({country: $(this).text() });
-    });
-  }
+    var $listCountries = $('#listCountries'),
+        $listCompanies = $('#listCompanies');
 
-  function selectCompany(filter) {
-  	//hide all
-  	$listCompanies.find('.tabCompany').hide().each(function() {
-  		if( $(this).find('td:eq(11)').text() === filter.country)
-  			$(this).show().find('tr:eq(5)').addClass('bg-success');
-  	});
+    function selectCompany(filter) {
+        //hide all
+        $listCompanies.find('.tabCompany').hide().each(function() {
+            if ($(this).find('td:eq(11)').text() === filter.country) $(this).show().find('tr:eq(5)').addClass('bg-success');
+        });
+    }
 
-
-  }
-
-    
-
-
-//TODO MAKE SELECT BOX FROM fertilizers.js
-
-  function initLogin() {
-
-    $('.protected').hide();
-
-    $('.btn-login').on('click', function(){
-        $('.protected').show();
-    });
-
-  }
-
-  return { init : init }
-
-})();
-
-window.addEventListener('load', fx_controller.init, false);
+	$listCountries.on('click', 'a', function(e) {
+		$(this).siblings().removeClass('btn-success active');
+		$(this).addClass('btn-success active');
+		selectCompany({
+		    country: $(this).text()
+		});
+	});
+});

b19080785930e041b477287b1b6384379d961e19
stefanocudini <stefano.cudini@gmail.com>
2015-02-10 13:08:00 +0100

    stats not work

--- statistics.js
+++ statistics.js
@@ -1,120 +1,132 @@
-require.config({
 
-    baseUrl: 'src/',
 
-    paths: {
-        'i18n': './lib/i18n',
-        'text': './lib/text',
-        'domready': './lib/domready',
-        'bootstrap': './lib/bootstrap',
-        //'backbone'              :'//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min',
-        'highcharts': '//fenixapps.fao.org/repository/js/highcharts/4.0.4/js/highcharts',
-        //'highcharts_exporting'  :'//fenixapps.fao.org/repository/js/highcharts/4.0.4/js/modules/exporting',
-        //'highcharts-heatmap'    :'http://code.highcharts.com/maps/modules/heatmap',
-        //'highcharts-data'       :'http://code.highcharts.com/maps/modules/data',
-        'jquery': './lib/jquery',
-        'underscore': './lib/underscore',
-        'jstree': './lib/jstree/jstree.min',
-        'handlebars': './lib/handlebars',
+require(["submodules/fenix-ui-menu/js/paths",
+		 "submodules/fenix-ui-common/js/Compiler"
+		 ], function(Menu, Compiler) {
 
-        //fenix-map-js
-/*        'fenix-map': '../submodules/fenix-map-js/dist/latest/fenix-map-min',
-        'fenix-map-config': '../submodules/fenix-map-js/dist/latest/fenix-map-config',
-*/      'chosen': '//fenixapps.fao.org/repository/js/chosen/1.0.0/chosen.jquery.min',
-        'leaflet': '//fenixapps.fao.org/repository/js/leaflet/0.7.3/leaflet',
-        'leaflet-markecluster': '//fenixapps.fao.org/repository/js/leaflet/plugins/leaflet.markecluster/1.1/leaflet.markercluster',
-        'import-dependencies': '//fenixapps.fao.org/repository/js/FENIX/utils/import-dependencies-1.0',
-        'jquery.power.tip': '//fenixapps.fao.org/repository/js/jquery.power.tip/1.1.0/jquery.powertip.min',
-        'jquery-ui': '//fenixapps.fao.org/repository/js/jquery-ui/1.10.3/jquery-ui-1.10.3.custom.min',
-        'jquery.i18n.properties': '//fenixapps.fao.org/repository/js/jquery/1.0.9/jquery.i18n.properties-min',
-        'jquery.hoverIntent': '//fenixapps.fao.org/repository/js/jquery.hoverIntent/1.0/jquery.hoverIntent',
-        'fenix-ui-topmenu': '../scripts/components/fenix-ui-topmenu',
+    var menuConfig = Menu;
+    
+    menuConfig['baseUrl'] = "submodules/fenix-ui-menu/js";
 
-/*        //OLAP
-        'pivot': '../submodules/fenix-ui-olap/js/pivot',
-        'countriesAgg': '//faostat3.fao.org/faostat-download-js/pivotAgg/countriesAgg',
-        'olap-config': '../prices/configuration',
-        'gt_msg_en': "../submodules/fenix-ui-olap/lib/grid/gt_msg_en",
-        //'gt_const': 'submodules/fenix-ui-olap/grid/gt_const',
-        'gt_grid_all': '../submodules/fenix-ui-olap/lib/grid/gt_grid_all',
-        'fusionchart': '../submodules/fenix-ui-olap/lib/grid/flashchart/fusioncharts/FusionCharts'
-*/
-    },
-
-    shim: {
-        'bootstrap': ['jquery'],
-        'chosen': ['jquery'],
-        'highcharts': ['jquery'],
-        'jstree': ['jquery'],
-        'jquery-ui': ['jquery'],
-        'jquery.power.tip': ['jquery'],
-        'jquery.i18n.properties': ['jquery'],
-        'jquery.hoverIntent': ['jquery'],
-        'underscore': {
-			exports: '_'
+    Compiler.resolve([menuConfig], {
+        placeholders : {
+            FENIX_CDN: "//fenixapps.fao.org/repository"
         },
-//		'fenix-map-config': {
-//			exports: 'FMCONFIG'
-//		},
-        'pivot': [
-			'jquery',
-			'jquery-ui',
-			'jquery.i18n.properties',
-			'countriesAgg',
-			'olap-config',
-			'gt_msg_en',
-			//'gt_const',
-			'gt_grid_all',
-			'fusionchart'
-        ],
-        'leaflet-markecluster': ['leaflet'],
-        'fenix-map': [
-			'i18n',
-			'jquery',
-			'chosen',
-			'leaflet',
-			'jquery-ui',
-			'jquery.hoverIntent',
-			'jquery.power.tip',
-			'jquery.i18n.properties',
-			'import-dependencies',
-			'fenix-map-config'
-        ]
-    }
-});
+        config: {
+			paths: {
+				'text': "//fenixapps.fao.org/repository/js/requirejs/plugins/text/2.0.12/text",
+				'i18n': "//fenixapps.fao.org/repository/js/requirejs/plugins/i18n/2.0.4/i18n",
+				'domready': "//fenixapps.fao.org/repository/js/requirejs/plugins/domready/2.0.1/domReady",
 
-require([
-        'jquery','underscore','bootstrap','highcharts','jstree','handlebars',
-        'leaflet',
-        'fenix-ui-topmenu/main',
-        'text!../config/services.json',
+				'amplify' : "//fenixapps.fao.org/repository/js/amplify/1.1.2/amplify.min",
+				'highcharts': "//fenixapps.fao.org/repository/js/highcharts/4.0.4/js/highcharts",
 
-        'text!../data/africa_countries.json',
-        'text!../data/africa_regions.json',
-        'text!../data/africa.json',
+				'underscore': "//fenixapps.fao.org/repository/js/underscore/1.7.0/underscore.min",
+				'handlebars': "//fenixapps.fao.org/repository/js/handlebars/2.0.0/handlebars",
 
-        'domready!'
-    ],
-    function ($, _, bts, highcharts, jstree, Handlebars, 
-		L,
-		TopMenu,
+				'domReady': "//fenixapps.fao.org/repository/js/requirejs/plugins/domready/2.0.1/domReady",
+				'swiper': "//fenixapps.fao.org/repository/js/swiper/2.7.5/dist/idangerous.swiper.min",
+				'bootstrap': "//fenixapps.fao.org/repository/js/bootstrap/3.3.2/js/bootstrap.min",
+				'draggabilly': "//fenixapps.fao.org/repository/js/draggabilly/dist/draggabilly.pkgd.min",
+				'intro': "//fenixapps.fao.org/repository/js/introjs/1.0.0/intro",
+				'isotope': "//fenixapps.fao.org/repository/js/isotope/2.1.0/dist/isotope.pkgd.min",
+				'jquery': "//fenixapps.fao.org/repository/js/jquery/2.1.1/jquery.min",
+				'jqwidgets': "//fenixapps.fao.org/repository/js/jqwidgets/3.1/jqx-light",
+				'jstree': "//fenixapps.fao.org/repository/js/jstree/3.0.8/dist/jstree.min",
+
+				//fenix-map-js
+				'fenix-map': "submodules/fenix-map-js/dist/latest/fenix-map-min",
+				'fenix-map-config': "submodules/fenix-map-js/dist/latest/fenix-map-config",
+				'chosen': "//fenixapps.fao.org/repository/js/chosen/1.0.0/chosen.jquery.min",
+				'leaflet': "//fenixapps.fao.org/repository/js/leaflet/0.7.3/leaflet",
+				'jquery.power.tip': "//fenixapps.fao.org/repository/js/jquery.power.tip/1.1.0/jquery.powertip.min",
+				'jquery-ui': "//fenixapps.fao.org/repository/js/jquery-ui/1.10.3/jquery-ui-1.10.3.custom.min",
+				'jquery.hoverIntent': "//fenixapps.fao.org/repository/js/jquery.hoverIntent/1.0/jquery.hoverIntent",
+				'jquery.i18n.properties': "//fenixapps.fao.org/repository/js/jquery/1.0.9/jquery.i18n.properties-min",
+				'import-dependencies': "//fenixapps.fao.org/repository/js/FENIX/utils/import-dependencies-1.0"
+			},
+
+		    shim: {
+		        'bootstrap': ['jquery'],
+		        'chosen': ['jquery'],
+		        'highcharts': ['jquery'],
+		        'jstree': ['jquery'],
+		        'jquery-ui': ['jquery'],
+		        'jquery.power.tip': ['jquery'],
+		        'jquery.i18n.properties': ['jquery'],
+		        'jquery.hoverIntent': ['jquery'],
+		        'underscore': {
+		            exports: '_'
+		        },
+                'amplify': {
+                    deps: ['jquery'],
+                    exports: 'amplifyjs'
+                },		        
+		        'fenix-map': {
+		            deps: [
+		                'i18n',
+		                'jquery',
+		                'chosen',
+		                'leaflet',
+		                'jquery-ui',
+		                'jquery.hoverIntent',
+		                'jquery.power.tip',
+		                'jquery.i18n.properties',
+		                'import-dependencies',
+		                'fenix-map-config'
+		            ]
+		        }	        
+		    }
+		}
+    });
+
+	require([
+	    'jquery', 'underscore', 'bootstrap', 'highcharts', 'jstree', 'handlebars', 'swiper', 'leaflet',
+	    'text!config/services.json',
+
+        'text!data/africa_regions.json',
+        'text!data/africa_regions_countries.json',        
+        'text!data/africa.json',
+
+		'fx-menu/start',
+        './scripts/components/AuthenticationManager',
+
+        'amplify',
+
+		'domready!'
+	], function($,_,bts,highcharts,jstree,Handlebars,Swiper,L,
 		Config,
-		
-		Countries,
+
 		Regions,
-		Africa) {
+		Countries,
+		Africa,
+
+		TopMenu,
+		AuthenticationManager
+		) {
 
 		Config = JSON.parse(Config);
+
+        new TopMenu({
+            active: 'statistics',        	
+            url: 'config/fenix-ui-menu.json',
+            className : 'fx-top-menu',
+            breadcrumb : {
+                active : true,
+                container : "#breadcumb_container",
+                showHome : true
+            }
+        });
+
+        new AuthenticationManager();
+        amplify.subscribe('login', function (user) {
+            console.warn("Event login intercepted");
+            console.log(amplify.store.sessionStorage('afo.security.user'));
+        });
+
 		Countries = JSON.parse(Countries);
 		Regions = JSON.parse(Regions);
 		Africa = JSON.parse(Africa);
-
-		console.log(Countries);
-
-		new TopMenu({
-			url: 'json/fenix-ui-topmenu_config.json',
-			active: "statistics"
-		});
 
 		var listRegions$ = $('#stats_select .stats_list_regions'),
 			listCountries$ = $('#stats_select .stats_list_countries'),
@@ -219,6 +231,7 @@ require([
 		};
 
 		var geomCountries = function(codes) {
+
 			var query = "SELECT ST_AsGeoJSON(ST_Transform(ST_SetSRID(geom, 3857), 4326)) "+
 						"FROM spatial.gaul0_faostat3_3857 "+
 						"WHERE faost_code IN ("+ codes.join(",") +")",
@@ -238,9 +251,15 @@ require([
 
 		var geomRegions = function(regCode) {
 
+			regCode = parseInt(regCode);
+
+console.log(regCode, Countries);
+
 			codes = _.filter(Countries, function(v) {
 				return v[0] == regCode;
 			});
+
+			console.log(codes);
 
 			codes = _.map(codes, function(v) {
 				return v[1];
@@ -274,8 +293,6 @@ require([
 		});
 
 		listRegions$.on('click', 'option', function(e) {
-			
-			//codes console.log( listCountries$.parent().val() )
 
 			var regCode = $(e.target).attr('value');
 			
@@ -284,8 +301,6 @@ require([
 		});
 
 		listCountries$.on('click', 'option', function(e) {
-			
-			//codes console.log( listCountries$.parent().val() )
 
 			var code = $(e.target).attr('value');
 			
@@ -294,5 +309,7 @@ require([
 		});
 
 
-	$('.footer').load('html/footer.html');		
+		$('.footer').load('html/footer.html');		
+	});
+
 });

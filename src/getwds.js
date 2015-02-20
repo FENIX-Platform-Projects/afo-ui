8822b8723fca33e4793488f77adddcd470405d29
stefanocudini <stefano.cudini@gmail.com>
2015-02-20 19:05:38 +0100

    update getwds component

--- src/getwds.js
+++ src/getwds.js
@@ -1,18 +1,17 @@
 
-define(['jquery','underscore','config'],function($,_) {
-	
+define(['jquery','underscore','config'],function($,_, Config, wdsConfig) {
+
+	function _template(str, data) {
+		return str.replace(/\{ *([\w_]+) *\}/g, function (str, key) {
+			return data[key] || '';
+		});
+	}
+
 	return function(queryTmpl, queryVars, callback) {
 
-		var sqltmpl, sql;`
-
-		if(queryVars) {
-			sqltmpl = _.template(queryTmpl);
-			sql = sqltmpl(queryVars);
-		}
-		else
-			sql = queryTmpl;
-
-		var	data = {
+		var ret,
+			sql = queryVars ? _template(queryTmpl, queryVars) : queryTmpl,
+			data = _.extend({
 				datasource: Config.dbName,
 				thousandSeparator: ',',
 				decimalSeparator: '.',
@@ -21,14 +20,28 @@ define(['jquery','underscore','config'],function($,_) {
 				nowrap: false,
 				valuesIndex: 0,
 				json: JSON.stringify({query: sql})
-			};
+			}, wdsConfig);
 
-		$.ajax({
-			url: Config.wdsUrl,
-			data: data,
-			type: 'POST',
-			dataType: 'JSON',
-			success: callback
-		});
+		if(_.isFunction(callback))
+			ret = $.ajax({
+				url: Config.wdsUrl,
+				data: data,
+				type: 'POST',
+				dataType: 'JSON',
+				success: callback
+			});
+		else
+			$.ajax({
+				async: false,
+				url: Config.wdsUrl,
+				data: data,
+				type: 'POST',
+				dataType: 'JSON',
+				success: function(resp) {
+					ret = resp;
+				}
+			});
+
+		return ret;
 	};
 });

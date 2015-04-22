077a66bc0254822f3fd5a6488afda1bcb72ab2aa
stefanocudini <stefano.cudini@gmail.com>
2015-04-23 01:08:54 +0200

    prices int chart png

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ require(["submodules/fenix-ui-menu/js/paths",
 			paths: {
 				'text': "//fenixapps.fao.org/repository/js/requirejs/plugins/text/2.0.12/text",
 				'i18n': "//fenixapps.fao.org/repository/js/requirejs/plugins/i18n/2.0.4/i18n",
-				'domready': "//fenixapps.fao.org/repository/js/requirejs/plugins/domready/2.0.1/domReady",
+				'domReady': "//fenixapps.fao.org/repository/js/requirejs/plugins/domready/2.0.1/domReady",
 
 				'amplify' : "//fenixapps.fao.org/repository/js/amplify/1.1.2/amplify.min",
 				'highcharts': "//fenixapps.fao.org/repository/js/highcharts/4.0.4/js/highcharts",
@@ -23,7 +23,6 @@ require(["submodules/fenix-ui-menu/js/paths",
 				'underscore': "//fenixapps.fao.org/repository/js/underscore/1.7.0/underscore.min",
 				'handlebars': "//fenixapps.fao.org/repository/js/handlebars/2.0.0/handlebars",
 
-				'domReady': "//fenixapps.fao.org/repository/js/requirejs/plugins/domready/2.0.1/domReady",
 				'swiper': "//fenixapps.fao.org/repository/js/swiper/2.7.5/dist/idangerous.swiper.min",
 				'bootstrap': "//fenixapps.fao.org/repository/js/bootstrap/3.3.2/js/bootstrap.min",
 				'draggabilly': "//fenixapps.fao.org/repository/js/draggabilly/dist/draggabilly.pkgd.min",

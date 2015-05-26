d531b2f2230fbb22a4ef88ba75728cd102a09f81
stefanocudini <stefano.cudini@gmail.com>
2015-05-26 13:06:16 +0200

    remove compoonents

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-copy');
+//	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-jsonlint');
 	grunt.loadNpmTasks('grunt-jsdoc');
 
@@ -49,96 +49,6 @@ module.exports = function(grunt) {
                 src: ['i18n/*.json']
             }
         },
-        copy: {
-            jquery: {
-                nonull: true,
-                src: 'node_modules/jquery/dist/jquery.min.js',
-                dest: 'src/lib/jquery.js'
-            },
-            backbone: {
-                nonull: true,
-                src: 'node_modules/backbone/backbone-min.js',
-                dest: 'src/lib/backbone.js'
-            },
-            "backbone.layoutmanager": {
-                nonull: true,
-                src: "node_modules/backbone.layoutmanager/backbone.layoutmanager.js",
-                dest: "src/lib/backbone.layoutmanager.js"
-            },
-            bootstrap_js: {
-                nonull: true,
-                src: "node_modules/bootstrap/dist/js/bootstrap.min.js",
-                dest: 'src/lib/bootstrap.js'
-            },
-            bootstrap_css: {
-                nonull: true,
-                src: "node_modules/bootstrap/dist/css/bootstrap.min.css",
-                dest: "src/lib/bootstrap.css"
-            },
-            nprogress_js: {
-                nonull: true,
-                src: "node_modules/nprogress/nprogress.js",
-                dest: "src/lib/nprogress.js"
-            },
-            nprogress_css: {
-                nonull: true,
-                src: "node_modules/nprogress/nprogress.css",
-                dest: "src/lib/nprogress.css"
-            },
-            requirejs: {
-                nonull: true,
-                src: "node_modules/requirejs/require.js",
-                dest: "src/lib/require.js"
-            },
-            domready: {
-                nonull: true,
-                src: "node_modules/domReady/domReady.js",
-                dest: "src/lib/domready.js"
-            },
-            text: {
-                nonull: true,
-                src: "node_modules/text/text.js",
-                dest: "src/lib/text.js"
-            },
-			i18n: {
-				nonull: true,
-				src: "node_modules/i18n/i18n.js",
-				dest: "src/lib/i18n.js"
-			},
-            underscore: {
-                nonull: true,
-                src: "node_modules/underscore/underscore-min.js",
-                dest: "src/lib/underscore.js"
-            },
-            underscorestring: {
-                nonull: true,
-                src: "node_modules/underscore.string/lib/underscore.string.js",
-                dest: "src/lib/underscore.string.js"
-            },
-            jqwidgets_js: {
-				nonull: true,
-				src: "node_modules/jqwidgets/jqx-all.js",
-				dest: "src/lib/jqwidgets.js"
-			},
-			jqwidgets_css: {
-				nonull: true,
-				src: "node_modules/jqwidgets/styles/jqx.base.css",
-				dest: "src/lib/jqwidegts.css"
-			},
-			handlebars: {
-				nonull: true,
-				src: "node_modules/handlebars/dist/handlebars.js",
-				dest: "src/lib/handlebars.js"
-			},
-			jstree: {
-				nonull: true,
-				expand: true,
-				//guit sflatten: true,
-				cwd: "node_modules/jstree/dist/",
-				src: '**',
-				dest: "src/lib/jstree/"
-			}		
-        },
         jsdoc: {
             dist: {
                 src: ["src/*.js"],
@@ -157,7 +67,6 @@ module.exports = function(grunt) {
     grunt.registerTask('default', [
         'clean',
         'jsonlint',
-        //'jshint',
-        'copy'
+        //'jshint'
     ]);
 };

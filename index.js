cea825b4bb9aa435ebd0f256acc3a99153923d6d
stefanocudini <stefano.cudini@gmail.com>
2015-05-26 11:54:48 +0200

    fenix cdn for require

--- index.js
+++ index.js
@@ -7,38 +7,38 @@ require(["submodules/fenix-ui-menu/js/paths",
 
     Compiler.resolve([menuConfig], {
         placeholders : {
-            FENIX_CDN: "//fenixapps.fao.org/repository"
+            FENIX_CDN: "//fenixrepo.fao.org/cdn"
         },
         config: {
 			paths: {
-				'text': "//fenixapps.fao.org/repository/js/requirejs/plugins/text/2.0.12/text",
-				'i18n': "//fenixapps.fao.org/repository/js/requirejs/plugins/i18n/2.0.4/i18n",
-				'domready': "//fenixapps.fao.org/repository/js/requirejs/plugins/domready/2.0.1/domReady",
+				'text': "{FENIX_CDN}/js/requirejs/plugins/text/2.0.12/text",
+				'i18n': "{FENIX_CDN}/js/requirejs/plugins/i18n/2.0.4/i18n",
+				'domready': "{FENIX_CDN}/js/requirejs/plugins/domready/2.0.1/domReady",
 
-				'amplify' : "//fenixapps.fao.org/repository/js/amplify/1.1.2/amplify.min",
-				'highcharts': "//fenixapps.fao.org/repository/js/highcharts/4.0.4/js/highcharts",
+				'amplify' : "{FENIX_CDN}/js/amplify/1.1.2/amplify.min",
+				'highcharts': "{FENIX_CDN}/js/highcharts/4.0.4/js/highcharts",
 
-				'underscore': "//fenixapps.fao.org/repository/js/underscore/1.7.0/underscore.min",
-				'handlebars': "//fenixapps.fao.org/repository/js/handlebars/2.0.0/handlebars",
+				'underscore': "{FENIX_CDN}/js/underscore/1.7.0/underscore.min",
+				'handlebars': "{FENIX_CDN}/js/handlebars/2.0.0/handlebars",
 
-				'swiper': "//fenixapps.fao.org/repository/js/swiper/2.7.5/dist/idangerous.swiper.min",
-				'bootstrap': "//fenixapps.fao.org/repository/js/bootstrap/3.3.2/js/bootstrap.min",
-				'draggabilly': "//fenixapps.fao.org/repository/js/draggabilly/dist/draggabilly.pkgd.min",
-				'intro': "//fenixapps.fao.org/repository/js/introjs/1.0.0/intro",
-				'isotope': "//fenixapps.fao.org/repository/js/isotope/2.1.0/dist/isotope.pkgd.min",
-				'jquery': "//fenixapps.fao.org/repository/js/jquery/2.1.1/jquery.min",
-				'jqwidgets': "//fenixapps.fao.org/repository/js/jqwidgets/3.1/jqx-light",
-				'jstree': "//fenixapps.fao.org/repository/js/jstree/3.0.8/dist/jstree.min",
+				'swiper': "{FENIX_CDN}/js/swiper/2.7.5/dist/idangerous.swiper.min",
+				'bootstrap': "{FENIX_CDN}/js/bootstrap/3.3.2/js/bootstrap.min",
+				'draggabilly': "{FENIX_CDN}/js/draggabilly/dist/draggabilly.pkgd.min",
+				'intro': "{FENIX_CDN}/js/introjs/1.0.0/intro",
+				'isotope': "{FENIX_CDN}/js/isotope/2.1.0/dist/isotope.pkgd.min",
+				'jquery': "{FENIX_CDN}/js/jquery/2.1.1/jquery.min",
+				'jqwidgets': "{FENIX_CDN}/js/jqwidgets/3.1/jqx-light",
+				'jstree': "{FENIX_CDN}/js/jstree/3.0.8/dist/jstree.min",
 
 				//fenix-map-js
 				'fenix-map': "submodules/fenix-ui-map/dist/fenix-ui-map.min",
 				'fenix-map-config': "submodules/fenix-ui-map/dist/fenix-ui-map-config",
-				'chosen': "//fenixapps.fao.org/repository/js/chosen/1.0.0/chosen.jquery.min",
-				'leaflet': "//fenixapps.fao.org/repository/js/leaflet/0.7.3/leaflet",
-				'jquery.power.tip': "//fenixapps.fao.org/repository/js/jquery.power.tip/1.1.0/jquery.powertip.min",
-				'jquery-ui': "//fenixapps.fao.org/repository/js/jquery-ui/1.10.3/jquery-ui-1.10.3.custom.min",
-				'jquery.hoverIntent': "//fenixapps.fao.org/repository/js/jquery.hoverIntent/1.0/jquery.hoverInten",
-				'jquery.i18n.properties': "//fenixapps.fao.org/repository/js/jquery/1.0.9/jquery.i18n.properties-min"
+				'chosen': "{FENIX_CDN}/js/chosen/1.0.0/chosen.jquery.min",
+				'leaflet': "{FENIX_CDN}/js/leaflet/0.7.3/leaflet",
+				'jquery.power.tip': "{FENIX_CDN}/js/jquery.power.tip/1.1.0/jquery.powertip.min",
+				'jquery-ui': "{FENIX_CDN}/js/jquery-ui/1.10.3/jquery-ui-1.10.3.custom.min",
+				'jquery.hoverIntent': "{FENIX_CDN}/js/jquery.hoverIntent/1.0/jquery.hoverInten",
+				'jquery.i18n.properties': "{FENIX_CDN}/js/jquery/1.0.9/jquery.i18n.properties-min"
 			},
 
 		    shim: {
@@ -64,7 +64,7 @@ require(["submodules/fenix-ui-menu/js/paths",
 		                'jquery',
 		                'chosen',
 		                'leaflet',
-		                'jquery-ui',
+		                //'jquery-ui',
 		                'jquery.hoverIntent',
 		                'jquery.power.tip',
 		                'jquery.i18n.properties',

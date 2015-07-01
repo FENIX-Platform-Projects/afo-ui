442a66b59852ede7d4344f67b5513fd832ab59f9
stefanocudini <stefano.cudini@gmail.com>
2015-07-01 14:56:31 +0200

    multisources

--- config/pivotConfig.js
+++ config/pivotConfig.js
@@ -1,26 +1,52 @@
 define({
-    "rows": [
-        "a1","a2","Unit" 
-    ],
-    "cols": ["a3"],
-    "vals": ["Value"  ],
-    "InstanceRenderers": [
-     {label: "Grid", func: "Table"},
-        {label: "TABLE", func: "Table2"},
-        {label: "barchart", func: "barchart"}
-    ],
-    "InstanceAggregators": [
-     {label: "SumUnit", func: "Sum2"},
-        {label: "Sum", func: "Sum"},
-        {label: "Average", func: "Average"}
-    ],
-     "hiddenAttributes": [
-       "Value"
-    ],
-    "showAgg": false,
-    "showRender": true,
-    "showUnit":false,
-    "showCode":false,
-    "showFlags":false
+
+    DEFAULT: {
+        "header":["code","Elements","Year","Value","Unit","Flag"],
+        "rows": [
+            "Elements","Unit"
+        ],
+        "cols": ["Year"],
+        "vals": ["Value"  ],
+        "InstanceRenderers": [
+            {label: "Grid", func: "Table"},
+            {label: "barchart", func: "barchart"}
+        ],
+        "InstanceAggregators": [
+            {label: "SumUnit", func: "Sum2"},
+            {label: "Sum", func: "Sum"},
+            {label: "Average", func: "Average"}
+        ],
+        "hiddenAttributes": ["Value","code","Flag"],
+        "showRender": true,
+        "showFlags": false,
+        "showUnit": false,
+        "showCode": false,
+        "showAgg": false
+    },
+
+    IFA: {
+        "header":["code", "Elements","Year","Nutrient","Value","Unit","Flag"],
+        "rows": [
+            "Elements","Nutrient","Unit" 
+        ],
+        "cols": ["Year"],
+        "vals": ["Value"  ],
+        "InstanceRenderers": [
+            {label: "Grid", func: "Table"},
+            {label: "barchart", func: "barchart"}
+        ],
+        "InstanceAggregators": [
+            {label: "SumUnit", func: "Sum2"},
+            {label: "Sum", func: "Sum"},
+            {label: "Average", func: "Average"}
+        ],
+        "hiddenAttributes": ["Value","Flag" ,"code"],
+        "linkedAttributes": [["Elements","Unit"]],
+        "showRender": true,
+        "showFlags": false,
+        "showUnit": false,
+        "showCode": false,
+        "showAgg": false
+    }
 });		
 //"NoRecords","RecordOrder","Domain Code","Domain","Country Code","Country","Element Code","Element","Item Code","Item","Year Code","Year","Unit","Value","Flag","Flag Description","Var1Order","Var2Order","Var3Order","Var4Order"
--- scripts/statistics/glance/Results.js
+++ scripts/statistics/glance/Results.js
@@ -15,33 +15,35 @@ define(['underscore',
     Pivot
     ) {
 
-    'use strict';
-
-    pivotConfig = _.extend(pivotConfig, {
-        rendererDisplay: pivotRenderers,
-        aggregatorDisplay: pivotAggregators
-    });
-
-    console.log('pivotConfig',pivotConfig);
-
-    var s = {
-        COURTESY: '#afo-courtesy',
-        RESULTS: '#afo-results'
-    };
-
     function Results() {
+
+        this.s = {
+            TABLE: "#table1",
+            COURTESY: '#afo-courtesy',
+            RESULTS: '#afo-results'
+        };        
         //this.table = new Table();
         this.pivot = new Pivot();
         //this.chart = new Chart();
     }
 
-    Results.prototype.printTable = function (data) {
+    Results.prototype.printTable = function (data, filter) {
 
-        data.unshift(["a1","a2","a3","Value","Unit"]);
+        var conf = pivotConfig.DEFAULT;
 
-        console.log('DAAAAATA',data);
+        if(filter.SOURCE[0].code === 'ifa') {
+            conf = pivotConfig.IFA;
+        }
 
-        this.pivot.render("table1",data, pivotConfig)
+        //add DATA HEADER
+        data.unshift(conf.header);   
+
+console.log(data, conf);
+
+        this.pivot.render(this.s.TABLE.replace('#',''), data, _.extend(conf, {
+            rendererDisplay: pivotRenderers,
+            aggregatorDisplay: pivotAggregators
+        }) );
     };
 
     Results.prototype.printChart = function (data) {

003e80892bf2e06a406da6182cf67c3e6290bf5e
stefanocudini <stefano.cudini@gmail.com>
2015-08-27 16:10:03 +0200

    pubs wdsclient

--- publications.js
+++ publications.js
@@ -18,140 +18,119 @@ require([
 	    'jquery', 'underscore', 'bootstrap', 'highcharts', 'jstree', 'handlebars', 'swiper',
 	    'config/services',
 	    'src/renderAuthMenu',
+	    'fx-common/js/WDSClient',
 		'text!html/publication.html'
 	], function($,_,bts,highcharts,jstree,Handlebars,Swiper,
 		Config,
 		renderAuthMenu,
-		tmplPublication) {
+		WDSClient,
+		tmplPublication
+	) {
 
 		renderAuthMenu(true);
 
-
-		function getWDS(queryTmpl, queryVars, callback) {
-
-			var sqltmpl, sql;
-
-			if(queryVars) {
-				sqltmpl = _.template(queryTmpl);
-				sql = sqltmpl(queryVars);
-			}
-			else
-				sql = queryTmpl;
-
-			var	data = {
-					datasource: Config.dbName,
-					thousandSeparator: ',',
-					decimalSeparator: '.',
-					decimalNumbers: 2,
-					cssFilename: '',
-					nowrap: false,
-					valuesIndex: 0,
-					json: JSON.stringify({query: sql})
-				};
-
-			$.ajax({
-				url: Config.wdsUrl,
-				data: data,
-				type: 'POST',
-				dataType: 'JSON',
-				success: callback
-			});
-		}
-
+		var wdsClient = new WDSClient({
+			datasource: Config.dbName,
+			outputType: 'array'
+		});
 
 		publicationTmpl = Handlebars.compile(tmplPublication);
 
 
 		//$.getJSON('data/publications.json', function(json) {	
 		
-		function getData(sql){getWDS(sql, null, function(json)	{
-			$('#listPubs').empty();
+		function getData(sql) {
 
-			var idPub = 0;
+			wdsClient.retrieve({
+				payload: {
+					query: sql
+				},
+				success: function(data) {
 
-			_.each(json, function(pub2) {
+					$('#listPubs').empty();
 
-				var pub = {
-					"PublicationId": idPub++,
-					"Category": pub2[0],
-					"PublicationName": pub2[1],
-					"PublicationDescription": pub2[2],
-					"PublicationSource": pub2[5],
-					"PublicationAuthorName": pub2[6],
-					"PublicationSector&Theme": pub2[7],
-					"PublicationDate":pub2[4],
-					"DocumentLanguage":pub2[8],
-					"REC":pub2[9],
-					"Countries":pub2[10],
-					"DocumentTags":pub2[15],
-					"PublicationRating":pub2[16],
-					"PublicationComments":"",
-					"DocumentType":pub2[11],
-					"DocumentSource":pub2[13]
-				};
+					var idPub = 0;
 
-				pub.DocumentTags = pub.DocumentTags ? pub.DocumentTags.split(', ') : '';
-				pub.Category = pub.Category ? pub.Category.split('|') : '';
-				pub.DocumentType = pub.DocumentType.replace('.','');
+					_.each(data, function(pub2) {
 
-				$('#listPubs').append( publicationTmpl(pub) );
+						var pub = {
+							"PublicationId": idPub++,
+							"Category": pub2[0],
+							"PublicationName": pub2[1],
+							"PublicationDescription": pub2[2],
+							"PublicationSource": pub2[5],
+							"PublicationAuthorName": pub2[6],
+							"PublicationSector&Theme": pub2[7],
+							"PublicationDate":pub2[4],
+							"DocumentLanguage":pub2[8],
+							"REC":pub2[9],
+							"Countries":pub2[10],
+							"DocumentTags":pub2[15],
+							"PublicationRating":pub2[16],
+							"PublicationComments":"",
+							"DocumentType":pub2[11],
+							"DocumentSource":pub2[13]
+						};
 
-			});		
-		});}
+						pub.DocumentTags = pub.DocumentTags ? pub.DocumentTags.split(', ') : '';
+						pub.Category = pub.Category ? pub.Category.split('|') : '';
+						pub.DocumentType = pub.DocumentType.replace('.','');
+
+						$('#listPubs').append( publicationTmpl(pub) );
+
+					});
+				}
+			});
+		}
 		
 		
 		getData(Config.queries.pubs_reformat);
-		Config.queries.pubs_reformat2=Config.queries.pubs_reformat;
+
+		Config.queries.pubs_reformat2 = Config.queries.pubs_reformat;
 
 
-		$("#txtSearch").on("input" ,function(){
-		$(".afo-category-list-li").removeClass("active");
-		$(".afo-category-list-li").addClass("noactive");
-		getData(Config.queries.pubs_reformat+" where upper(description) like '%"+this.value.toUpperCase().split(" ").join("%")+"%' or upper(title) like '%"+this.value.toUpperCase().split(" ").join("%")+"%' or upper(author_name) like '%"+this.value.toUpperCase().split(" ").join("%")+"%' or upper(source) like '%"+this.value.toUpperCase().split(" ").join("%")+"%'");
-		
-		Config.queries.pubs_reformat2=Config.queries.pubs_reformat+" where upper(description) like '%"+this.value.toUpperCase().split(" ").join("%")+"%' or upper(title) like '%"+this.value.toUpperCase().split(" ").join("%")+"%' or upper(author_name) like '%"+this.value.toUpperCase().split(" ").join("%")+"%' or upper(source) like '%"+this.value.toUpperCase().split(" ").join("%")+"%' "
-		});
-		
-		
-		/*getWDS("select * from publications",null,function(data)	{
-		console.log(data);
-		});*/
-		
-		
-		$(".afo-category-list-li").click(function(){
-		$(".afo-category-list-li").removeClass("active");
-		$(".afo-category-list-li").addClass("noactive");
-		//console.log(this.innerHTML)
-		document.getElementById("txtSearch").value="";
-		if(this.innerHTML=="All")
-		{
-			getData(Config.queries.pubs_reformat);
-		Config.queries.pubs_reformat2=Config.queries.pubs_reformat;
-		
+		$("#txtSearch").on("input", function() {
+			$(".afo-category-list-li").removeClass("active");
+			$(".afo-category-list-li").addClass("noactive");
+
+			getData(Config.queries.pubs_reformat+" where upper(description) like '%"+this.value.toUpperCase().split(" ").join("%")+"%' or upper(title) like '%"+this.value.toUpperCase().split(" ").join("%")+"%' or upper(author_name) like '%"+this.value.toUpperCase().split(" ").join("%")+"%' or upper(source) like '%"+this.value.toUpperCase().split(" ").join("%")+"%'");
 			
-		}
-		else{
-		getData(Config.queries.pubs_reformat+" where upper(category) like '%"+this.innerHTML.toUpperCase()+"%' ");
-		Config.queries.pubs_reformat2=Config.queries.pubs_reformat+" where upper(category) like '%"+this.innerHTML.toUpperCase()+"%' ";
-		}
-		//console.log(Config.queries.pubs_reformat+" where upper(category) like '%"+this.innerHTML.toUpperCase()+"%' ")
-		this.className="afo-category-list-li active";
+			Config.queries.pubs_reformat2 = Config.queries.pubs_reformat+" where upper(description) like '%"+this.value.toUpperCase().split(" ").join("%")+"%' or upper(title) like '%"+this.value.toUpperCase().split(" ").join("%")+"%' or upper(author_name) like '%"+this.value.toUpperCase().split(" ").join("%")+"%' or upper(source) like '%"+this.value.toUpperCase().split(" ").join("%")+"%' ";
+		});		
+		
+		$(".afo-category-list-li").on('click', function() {
+			$(".afo-category-list-li").removeClass("active");
+			$(".afo-category-list-li").addClass("noactive");
+			//console.log(this.innerHTML)
+			$("#txtSearch").val('');
+			if(this.innerHTML=="All")
+			{
+				getData(Config.queries.pubs_reformat);
+				Config.queries.pubs_reformat2 = Config.queries.pubs_reformat;
+			}
+			else {
+				getData(Config.queries.pubs_reformat+" where upper(category) like '%"+this.innerHTML.toUpperCase()+"%' ");
+				Config.queries.pubs_reformat2 = Config.queries.pubs_reformat+" where upper(category) like '%"+this.innerHTML.toUpperCase()+"%' ";
+			}
+			//console.log(Config.queries.pubs_reformat+" where upper(category) like '%"+this.innerHTML.toUpperCase()+"%' ")
+			this.className="afo-category-list-li active";
 		});
 		
-		$("#mostRecentOreder").click(function(){
+		$("#mostRecentOreder").on('click', function(){
 		
-		getData(Config.queries.pubs_reformat2 +"  order by posting_date DESC");
-		
-		});
-		
-		$("#alphabeticOrder").click(function(){
-		
-		getData(Config.queries.pubs_reformat2 +"  order by title");
+			getData(Config.queries.pubs_reformat2 +"  order by posting_date DESC");
 		
 		});
-		$("#alphabeticOrderInv").click(function(){
 		
-		getData(Config.queries.pubs_reformat2 +"  order by title DESC");
+		$("#alphabeticOrder").on('click', function(){
+		
+			getData(Config.queries.pubs_reformat2 +"  order by title");
+		
+		});
+
+		$("#alphabeticOrderInv").on('click', function(){
+		
+			getData(Config.queries.pubs_reformat2 +"  order by title DESC");
 		
 		});	
 		
